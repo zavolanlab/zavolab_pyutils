df819992ed875501c00c8d4fb2e6b9b59e81573b
aleksei.mironov.scicore <magmir71@gmail.com>
2026-03-24 14:06:15 +0100

    modify deseq2_normalize

--- src/zavolab_pyutils/read_count_data_analysis.py
+++ src/zavolab_pyutils/read_count_data_analysis.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import numpy as np
-import logging
-
 """
 Utilities for analyzing read count data stored as pandas DataFrames.
 Includes library size normalization, mean-variance relationship analysis,
@@ -9,6 +5,8 @@ funtions for differential expression testing and differetial relative usage test
 (e.g. alternative splicing, AS and alternative polyadenylation, APA).
 """
 
+import pandas as pd
+import numpy as np
 
 def deseq2_normalize(counts_df, sample_list, lowExprGenesQ=0.3, pseudocount = 1):
     """
@@ -19,7 +17,7 @@ def deseq2_normalize(counts_df, sample_list, lowExprGenesQ=0.3, pseudocount = 1)
     Parameters
     ----------
     counts_df : pd.DataFrame
-        Raw count matrix (genes x samples).
+        Raw count matrix (genes x samples). May contain any additional columns, e.g. gene names, gene lengths etc.
     sample_list : list
         List of column names in counts_df to normalize.
     lowExprGenesQ: float
@@ -34,30 +32,34 @@ def deseq2_normalize(counts_df, sample_list, lowExprGenesQ=0.3, pseudocount = 1)
     sfs_df : pd.DataFrame
         Dataframe containing calculated size factors and read sums.
     """
+
+    # check that all samples in sample_list are present in counts_df
+    missing_samples = [s for s in sample_list if s not in counts_df.columns]
+    if missing_samples:
+        raise ValueError(f"Missing samples in counts_df: {missing_samples}")
+
     # Work on a copy to avoid SettingWithCopy warnings on the original df
     # We only look at the specific samples requested
     df_work = counts_df[sample_list].copy()
 
-    # 1. Calculate Geometric Means
+    # 1. Filter low-expressed genes from Size Factor calculation
     # We use log-space to prevent overflow: geometric_mean = 2**(mean(log(x)))
     # We only care about genes with >0 counts in ALL samples for the reference
-    valid_genes_mask = (df_work > 0).all(axis=1)
+    df_work['mean'] = df_work.mean(axis=1)
+    threshold = max(df_work['mean'].quantile(lowExprGenesQ),0)
+    high_expr_genes = df_work[(df_work['geom_mean'] > threshold)&(df_work[sample_list].min(axis=1) > 0)].index
     
-    # Slice only valid genes
-    ref_genes_df = df_work.loc[valid_genes_mask].copy()
+    # Slice only highly expressed genes
+    ref_genes_df = df_work.loc[high_expr_genes].copy()
     
-    # Calculate Log Geometric Mean
-    log_counts = np.log2(ref_genes_df)
+    # 2. Calculate Log Geometric Mean
+    log_counts = np.log2(ref_genes_df[sample_list]) # we don't need pseudocount here since we already filtered out zero-count genes
     log_geom_means = log_counts.mean(axis=1)
-    
-    # 2. Filter low-expressed genes from Size Factor calculation
-    threshold = log_geom_means.quantile(lowExprGenesQ)
-    high_expr_genes = log_geom_means[log_geom_means > threshold].index
     
     # 3. Calculate Size Factors (Median of Ratios)
     # Ratio = Count / GeometricMean
     # We use the filtered high-expression set
-    ratios = log_counts.loc[high_expr_genes].sub(log_geom_means.loc[high_expr_genes], axis=0)
+    ratios = log_counts.sub(log_geom_means, axis=0)
     log2_sf_series = ratios.median(axis=0)
 
     # 4. Create the Size Factor DataFrame
@@ -74,7 +76,7 @@ def deseq2_normalize(counts_df, sample_list, lowExprGenesQ=0.3, pseudocount = 1)
     sfs_df['read_sum_mln'] = np.round(sfs_df['read_sum'] / 1e6, 2)
     
     # 6. Apply Normalization to the original matrix
-    # Divide original counts by the size factors
+    # Apply pseudocount to original counts and divide by the size factors
     norm_counts = (df_work[sample_list]+pseudocount).div(sfs_df['sf'], axis=1)
     
     return norm_counts, sfs_df
